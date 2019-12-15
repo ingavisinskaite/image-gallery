25f6696829b1274385f38ac8ccc7f8cdc8afc9c6
ingavisinskaite <ingavisinskaite@gmail.com>
2019-12-15 13:04:14 +0200

    funtion to enlarge image

--- image-gallery.js
+++ image-gallery.js
@@ -1,17 +1,29 @@
 let url = "https://picsum.photos/v2/list"
 
-createImage = (url) => {
+createImage = (img) => {
     image = document.createElement('img');
     imagesContainer = document.getElementById('container');
-    image.id = "image"
-    image.src = url;
+    image.classList.add("image");
+    image.src = img.download_url;
+    image.addEventListener('click', e => {enlargeImage(img), false});
     imagesContainer.appendChild(image);
+}
+
+enlargeImage = (img) => {
+    enlargedImage = document.createElement('img');
+    enlargedImageContainer = document.getElementById('enlarged-image-container');
+    imageInfo = document.createElement('p');
+    enlargedImage.src = img.download_url;
+    enlargedImage.id = 'enlarged-image';
+    imageInfo.innerHTML = `${img.author} ${img.height}x${img.width}`;
+    enlargedImageContainer.appendChild(enlargedImage);
+    enlargedImageContainer.appendChild(imageInfo);
 }
 
 fetch(url)
 .then( resp => resp.json())
 .then( data => {
     data.forEach(image => {
-        createImage(image.url)
+        createImage(image)
     })
 })
