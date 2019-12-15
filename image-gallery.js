974ced39f60bf5cec3ec271f8a5c733b7babdf07
ingavisinskaite <ingavisinskaite@gmail.com>
2019-12-15 23:38:43 +0200

    shortened up code

--- image-gallery.js
+++ image-gallery.js
@@ -22,28 +22,14 @@ createImage = img => {
 };
 
 enlargeImage = img => {
-  let imageGallery = document.getElementById("image-gallery");
-  if (imageGallery.childNodes.length > 1) {
-    imageGallery.lastChild.remove();
-  }
+  let enlargedImageContainer = document.getElementById('enlarged-image-container');
+  let enlargedImage = document.getElementById('enlarged-image');
+  let imageAuthor = document.getElementById('image-author')
+  let imageSize = document.getElementById('image-size');
 
-  let enlargedImageContainer = document.createElement("div");
-  let enlargedImage = document.createElement("img");
-  let imageInfo = document.createElement("div");
-  let imageAuthor = document.createElement("span");
-  let imageSize = document.createElement("span");
-
-  enlargedImageContainer.id = "enlarged-image-container";
   enlargedImage.src = img.download_url;
-  enlargedImage.id = "enlarged-image";
-  imageAuthor.id = "image-author";
-  imageSize.id = "image-size";
   imageAuthor.innerHTML = img.author;
   imageSize.innerHTML = ` ${img.height}x${img.width}`;
 
-  imageInfo.appendChild(imageAuthor);
-  imageInfo.appendChild(imageSize);
-  enlargedImageContainer.appendChild(enlargedImage);
-  enlargedImageContainer.appendChild(imageInfo);
-  imageGallery.appendChild(enlargedImageContainer);
+  enlargedImageContainer.style.display = "block";
 };
